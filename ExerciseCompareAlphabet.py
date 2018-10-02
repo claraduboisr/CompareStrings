521b49f5070d6fe8f0dc2c8e02acda3bbed0775f
claraduboisr <claraduboisr@gmail.com>
2018-10-02 10:58:46 +0200

    Initial

--- ExerciseCompareAlphabet.py
+++ ExerciseCompareAlphabet.py
@@ -1,1 +1,13 @@
-s = 
+s = "acebdfz"
+
+long = ""
+solution = ""
+pre = "a"
+# smallest character
+
+for c in s:
+    #compare third character to s
+    if c > pre:
+        solution = solution+c
+
+    else:
